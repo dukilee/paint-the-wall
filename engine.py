cf3831a0117f32a6834a3c6a330ac8b0dbc7a720
dukilee <lekdekum@gmail.com>
2017-03-13 23:55:19 -0300

    Improved performance

--- engine.py
+++ engine.py
@@ -30,13 +30,24 @@ class Engine:
 					self.grid[nx][ny] = constants.NOTHING
 					stack.append([nx, ny])
 
+	def getColor(self, n):
+		if n == constants.PROCESS:
+			return constants.LIGHT_GREEN
+		if n == constants.CONQUERED:
+			return constants.GREEN
+		if n == constants.HYPER:
+			return constants.GREEN
+		return constants.WHITE
+
 	def run(self, screen):
 		self._ball = []
+		conquering = False #true if there is any process block
+		repint = True#true when the hole screen needs to be redrawn
+		objectErase = []#marks the position of everything that surrounds objects(balls or hero)
 		for i in range(self.numberBalls):
 			self._ball.append(ball.Ball(i))
 
 		for i in range(self.numberBalls):
-			#print("iniciando")	
 			self._ball[i].pos.print()
 
 		pygame.init()
@@ -49,14 +60,16 @@ class Engine:
 		for i in range (constants.GRID_SIZE[1]):
 			self.grid[0][i] = constants.CONQUERED
 			self.grid[constants.GRID_SIZE[0]-1][i] = constants.CONQUERED
-		
+
 		while not doneRunning:
+
 			#handle player input
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					sys.exit()
 				if event.type == pygame.KEYDOWN:
 					if event.key == constants.KEY_q:
+						sys.exit()
 						doneRunning = True
 					else:
 						self._hero.update(event.key, True)
@@ -69,40 +82,78 @@ class Engine:
 				if self._ball[i].update(self.grid) == constants.LOSE:
 					return constants.LOSE
 
+			#draw ball
+			for i in range(self.numberBalls):
+				pygame.draw.circle(screen, constants.BLUE, [self._ball[i].pos.x + constants.BALL_RADIUS, self._ball[i].pos.y + constants.BALL_RADIUS], constants.BALL_RADIUS)
+				
+
+			#calculates what parts of the image needs to be redone
+			objectErase = [] 
+			_heroPos = vector2.Vector2(int(round(self._hero.pos.x/constants.SCALE[0])), int(round(self._hero.pos.y/constants.SCALE[1])))
+			for i in [-1, 0, 1]:
+				for j in [-1, 0, 1]:
+					if _heroPos.x+i<0 or _heroPos.x+i>=constants.GRID_SIZE[0] or _heroPos.y+j<0 or _heroPos.y+j>=constants.GRID_SIZE[1]:
+						continue
+					r = Rect((_heroPos.x+i)*constants.SCALE[0], (_heroPos.y+j)*constants.SCALE[1], constants.SCALE[0], constants.SCALE[1])
+					objectErase.append(r)
+					color = self.getColor(self.grid[(_heroPos.x+i)][_heroPos.y+j])
+					pygame.draw.rect(screen, color, r)
+
+			for balls in self._ball: 
+				_ballPos = vector2.Vector2(int(round(balls.pos.x/constants.SCALE[0])), int(round(balls.pos.y/constants.SCALE[1])))
+				for i in [-1, 0, 1]:
+					for j in [-1, 0, 1]:
+						if _ballPos.x+i<0 or _ballPos.x+i>=constants.GRID_SIZE[0] or _ballPos.y+j<0 or _ballPos.y+j>=constants.GRID_SIZE[1]:
+							continue
+						color = self.getColor(self.grid[(_ballPos.x+i)][_ballPos.y+j])
+						if color != constants.WHITE:
+							continue
+						r = Rect((_ballPos.x+i)*constants.SCALE[0], (_ballPos.y+j)*constants.SCALE[1], constants.SCALE[0], constants.SCALE[1])
+						objectErase.append(r)
+						pygame.draw.rect(screen, color, r)
+
+
+
+
 			#update grid 
 			_heroPos = vector2.Vector2(int(round(self._hero.pos.x/constants.SCALE[0])), int(round(self._hero.pos.y/constants.SCALE[1])))
 			if self.grid[_heroPos.x][_heroPos.y] == constants.CONQUERED:
-				for y in range(constants.GRID_SIZE[1]):
-					for x in range(constants.GRID_SIZE[0]):
-						if self.grid[x][y] == constants.NOTHING:
-							self.grid[x][y] = constants.HYPER
-						elif self.grid[x][y] == constants.PROCESS:
-							self.grid[x][y] = constants.CONQUERED
-				for i in range(self.numberBalls):
-					self.DFS(int(round(self._ball[i].pos.x/constants.SCALE[0])), int(round(self._ball[i].pos.y/constants.SCALE[1])))
+				if conquering:
+					conquering = False
+					repint = True
+					for y in range(constants.GRID_SIZE[1]):
+						for x in range(constants.GRID_SIZE[0]):
+							if self.grid[x][y] == constants.NOTHING:
+								self.grid[x][y] = constants.HYPER
+							elif self.grid[x][y] == constants.PROCESS:
+								self.grid[x][y] = constants.CONQUERED
+					for i in range(self.numberBalls):
+						self.DFS(int(round(self._ball[i].pos.x/constants.SCALE[0])), int(round(self._ball[i].pos.y/constants.SCALE[1])))
 			else:
+				conquering = True
 				self.grid[_heroPos.x][_heroPos.y] = constants.PROCESS
 			
 			contador = int(3*constants.GRID_SIZE[0]*constants.GRID_SIZE[1]/4)			
 
 			#draw grid
-			for x in range(constants.GRID_SIZE[0]):
-				for y in range(constants.GRID_SIZE[1]):
-					_color = constants.WHITE
-					if self.grid[x][y] == constants.PROCESS:
-						_color = constants.LIGHT_GREEN
-					elif self.grid[x][y] == constants.CONQUERED:
-						_color = constants.GREEN
-						contador -= 1
-					elif self.grid[x][y] == constants.HYPER:
-						_color = constants.GREEN
-						self.grid[x][y] = constants.CONQUERED
-					pygame.draw.rect(screen, _color, [x*constants.SCALE[0], y*constants.SCALE[1], constants.SCALE[0], constants.SCALE[1]])
-
+			if repint:
+				repint = False
+				for x in range(constants.GRID_SIZE[0]):
+					for y in range(constants.GRID_SIZE[1]):
+						_color = constants.WHITE
+						if self.grid[x][y] == constants.PROCESS:
+							_color = constants.LIGHT_GREEN
+						elif self.grid[x][y] == constants.CONQUERED:
+							_color = constants.GREEN
+							contador -= 1
+						elif self.grid[x][y] == constants.HYPER:
+							_color = constants.GREEN
+							self.grid[x][y] = constants.CONQUERED
+						pygame.draw.rect(screen, _color, [x*constants.SCALE[0], y*constants.SCALE[1], constants.SCALE[0], constants.SCALE[1]])
+				pygame.display.flip()
 			#win condition
 			if contador <= 0:
 				print("YOU WON :)")
-				#sys.exit()
 				return constants.WIN
 
 			#draw hero
@@ -116,6 +167,6 @@ class Engine:
 			font = pygame.font.SysFont('Calibri', 25, True, False)
 			text = font.render("Left: {}".format(contador), True, constants.BLACK)
 			screen.blit(text, [0, 0])
-
-			pygame.display.flip()
+			pygame.display.update(objectErase)
+	
 			clock.tick(100)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pygame
 from pygame.locals import *
 
 pygame.init()
+pygame.display.set_caption('THE GAME', 'The Game')
 music = pygame.mixer.music
 screen = pygame.display.set_mode(constants.SCREEN_SIZE)
 
@@ -12,20 +13,20 @@ _menu = menu.Menu()
 action = constants.UNDEFINED
 
 while action != constants.QUIT:
-	music.load('sounds/teste_1.mid')
-	music.play(0)
+#	music.load('sounds/teste_1.mid')
+#	music.play(0)
 	
-	action = _menu.update(screen)
-	music.stop()
+	# action = _menu.update(screen)
+#	music.stop()
 
-	music.load('sounds/teste_2.mid')
-	music.play(0)
-	if action == constants.STAGE_SELECT:
+#	music.load('sounds/teste_2.mid')
+#	music.play(0)
+	# if action == constants.STAGE_SELECT:
 		_engine = engine.Engine()
-		_engine.numberBalls = 4
+		_engine.numberBalls = 12
 		_engine.run(screen)
 		action = constants.UNDEFINED
-	elif action == constants.LEVEL_0:
-		pass
+	# elif action == constants.LEVEL_0:
+		# pass
 
-	music.stop()
+	# music.stop()
