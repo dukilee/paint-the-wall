9b4d334a6d6599f9490e356f75df28425cc90c40
Thiago-Filipe <thiagoofic@gmail.com>
2017-04-09 19:48:32 -0300

    Now data is stored as a encripted bin file
    - Harder to cheat just by modding the "data" file, but still needing a
    better encription method;

--- user_data/dataManager.py
+++ user_data/dataManager.py
@@ -1,20 +1,25 @@
+import binascii
+
 from user_data import data
 
 class DataManager:
 	def __init__(self):
 		try:
-			file = open('user_data\data.txt', 'r')
+			file = open('user_data\\user_data.bin', 'rb')
 			
-			content = file.readlines()
-			for x in content:
-				lines = x.strip().split(": ")
+			content = (self.read_data(file)).split('\n')
+			file.close()			
 
+			for x in content:
+				if x == '':
+					break
+
+				lines = x.strip().split(": ")
+				
 				if len(lines) != 2 or lines[0] not in data.i.keys():
 					print("Data file is corrupted :(")
 				else:
 					data.i[lines[0]] = int(float(lines[1]))
-
-			file.close()
 
 		except IOError:
 			print("Creating a new data file...")
@@ -24,8 +29,21 @@ class DataManager:
 		return val
 
 	def save(self):
-		file = open('user_data\data.txt', 'w')
+		file = open('user_data\\user_data.bin', 'wb')
 		for name, val in data.i.items():
 			s = name + ": " + str(val) + "\n"
-			file.write(s)
+			self.write_data(file, s)
 		file.close()
+
+	def write_data(self, file, s):
+		for k in s:
+			file.write(bytes([255 - ord(k)])) #encript
+			file.flush()
+
+	def read_data(self, file):
+		s = ''
+		x = file.read(1)
+		while x != b'':
+			s += chr(255 - int(binascii.hexlify(x), 16)) #decript
+			x = file.read(1)
+		return s
